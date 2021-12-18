a1c0717cb7fe251f00679399003ee3656fd480ff
brown <ohbass95@gmail.com>
2021-12-18 23:28:23 +0900

    D03

---
+++ Ex/TestScanner.java
@@ -0,0 +1,14 @@
+package Ex;
+	import java.util.Scanner; 
+	
+public class TestScanner {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);	
+		
+		String input = scanner.nextLine();
+		int num = Integer.parseInt(input);
+
+	}
+
+}
