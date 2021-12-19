b97952435d203f4334ccf350edd61472ddd502aa
brown <ohbass95@gmail.com>
2021-12-19 23:48:14 +0900

    D04

---
+++ Ex/Ex2_12.java
@@ -0,0 +1,14 @@
+package Ex;
+//타입 간의 변환방법
+
+public class Ex2_12 {
+	public static void main(String[] args) {
+		String str = "3";
+		
+		System.out.println(str.charAt(0) - '0');
+		System.out.println('3' - '0' + 1);
+		System.out.println(Integer.parseInt("3")+ 1);
+		System.out.println("3" + 1);
+		System.out.println((char)(3 + '0'));
+	}
+}
--- Ex/TestScanner.java
+++ Ex/TestScanner.java
@@ -1,14 +1,16 @@
 package Ex;
-	import java.util.Scanner; 
+	import java.util.Scanner; // Scanner 클래스를 사용하기 ㅜ이해 추가
 	
 public class TestScanner {
 	public static void main(String[] args) {
 		
-		Scanner scanner = new Scanner(System.in);	
+		Scanner scanner = new Scanner(System.in);	//Scanner 클ㄹ래스의 객체를 생성
 		
-		String input = scanner.nextLine();
-		int num = Integer.parseInt(input);
-
+		String input = scanner.nextLine();	//입력받은 내용을 input에 저장
+		int num = Integer.parseInt(input);	//입력받은 내용을 int타입의의 값으로 변환
+		
+		int num1 = scanner.nextInt();	//정수를 입력받아서 변수 num에 저장
+		
 	}
 
 }
